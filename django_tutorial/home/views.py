03bfb7e5405ead469c856f35497d0f7cc9e323b4
jalala-pv <pvjalala@gmail.com>
2025-02-06 22:01:17 +0530

    pagescreated

--- django_tutorial/home/views.py
+++ django_tutorial/home/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("home")
+def about(request):
+    return HttpResponse("about page")
+def booking(request):
+    return HttpResponse("booking page")
+def doctors(request):
+    return HttpResponse("doctors")
+def contact(request):
+    return HttpResponse("contact")
