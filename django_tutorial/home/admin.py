6b7f17fa1c9ad1b0da1c34c3ee4fe75fdae84929
jalala-pv <pvjalala@gmail.com>
2025-02-11 17:26:31 +0530

    deptmodelregister

--- django_tutorial/home/admin.py
+++ django_tutorial/home/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from . models import Departments
+admin.site.register(Departments)
 # Register your models here.
--- django_tutorial/home/models.py
+++ django_tutorial/home/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+class Departments(models.Model):
+    dept_name=models.CharField(max_length=100)
+    dept_description=models.TextField()
 
-# Create your models here.
