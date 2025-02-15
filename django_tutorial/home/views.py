4e5373cfce3e4a5f5dc1ff28b5ddead84075f03f
jalala-pv <pvjalala@gmail.com>
2025-02-15 21:38:17 +0530

    Doctorpage

--- django_tutorial/home/views.py
+++ django_tutorial/home/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from . models import Departments
+from . models import Departments,Doctors
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -9,7 +9,10 @@ def about(request):
 def booking(request):
     return render(request,'booking.html')
 def doctors(request):
-    return render(request,'doctors.html')
+    dict_docs={
+        'doctors':Doctors.objects.all()
+    }
+    return render(request,'doctors.html',dict_docs)
 def contact(request):
     return render(request,'contact.html')
 def department(request):
