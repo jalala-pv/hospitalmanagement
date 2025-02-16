03cf4eca32b06cdb75afe98758a06b76c9f54173
jalala-pv <pvjalala@gmail.com>
2025-02-16 07:46:42 +0530

    bookingmodel

--- django_tutorial/home/admin.py
+++ django_tutorial/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Departments,Doctors
+from . models import Departments,Doctors,Booking
 admin.site.register(Departments)
 admin.site.register(Doctors)
+admin.site.register(Booking)
 
 # Register your models here.
